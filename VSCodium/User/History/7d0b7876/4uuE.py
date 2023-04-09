9b5de6265a7f2ca192aa83bade0cca18a4e55a2a
nxtperfect <alakaxan@protonmail.com>
2023-04-09 21:36:21 +0200

    09.04

---
+++ VSCodium/User/History/7d0b7876/4uuE.py
@@ -0,0 +1,3 @@
+import unittest
+import main
+# check position of the cursor, move it, then check position agian, if it changed how it should, test passed
--- polybar/env.sh
+++ polybar/env.sh
@@ -1,4 +1,4 @@
 
-export REDSHIFT=off
+export REDSHIFT=on
 export REDSHIFT_TEMP=1100
 export REDSHIFT_BRIGHTNESS=0.6
