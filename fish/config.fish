dd9a6f4fe4337f0a802925ff27d48f80a5afad2a
NxtPerfect <alakaxan@protonmail.com>
2024-05-05 07:32:40 +0200

    05.05.2024
    Fedora 38

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ if status is-interactive
 end
 
 alias update="sudo dnf update -y && sudo flatpak update -y"
-alias flex="clear && neofetch --ascii_distro redstar"
+alias flex="clear && hyfetch"
 # alias update-grub="sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB && sudo grub-mkconfig -o /boot/grub/grub.cfg"
 alias rm="trash"
 alias v="nvim"
@@ -13,3 +13,5 @@ alias v="nvim"
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 set -gx EDITOR nvim
+
+zoxide init --cmd cd fish | source
